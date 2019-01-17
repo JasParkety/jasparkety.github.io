5cde051ccf06e039c0a5189b3328a2aefd820aa6
Lukas Hausner <lukas.hausner@qitasc.com>
2019-01-17 16:21:10 +0100

    Refreshing for .gitignore

--- _site/assets/js/usecases.js
+++
@@ -1,5 +0,0 @@
-$(".image-container").mouseover(function (e) {
-    $(this).attr("src", $(this).attr("src").replace(".png", "_hover.png"));
-}).mouseout(function (e) {
-    $(this).attr("src", $(this).attr("src").replace("_hover.png", ".png"));
-});
