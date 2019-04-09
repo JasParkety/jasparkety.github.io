3c06af26190e917ef970ec7063f9ca626c79a6ec
lukas.hausner <lukas.hausner@qitasc.com>
2019-04-09 12:18:30 +0200

    master purge

--- _hidden/assets/js/usecases.js
+++
@@ -1,7 +0,0 @@
-$(".usecaseIndividualContainer").mouseover(function (e) {
-    $(this).find(".image-container").attr("src", $(this).find(".image-container").attr("src").replace(".png", "_hover.png"));
-    $(this).find(".usecaseHeader").css("color", "#f7941e");
-}).mouseout(function (e) {
-    $(this).find(".image-container").attr("src", $(this).find(".image-container").attr("src").replace("_hover.png", ".png"));
-    $(this).find(".usecaseHeader").css("color", "#64646a");
-});
